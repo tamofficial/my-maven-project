a6699fcb5ac77afdd475977b2d35b3569a6b7796
Tamerlan <tamerlan2636@gmail.com>
2025-04-14 21:01:18 +0300

    Проект собран, тесты работают

---
+++ src/tamerlan/src/test/java/CalculatorTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calc = new Calculator();
+        int result = calc.add(2, 3);
+        assertEquals(5, result);
+    }
+}
